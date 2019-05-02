257b80ab5f4b8972074816a1ca296aeb9ba75d5c
Adhocmaster <adhocmaster@live.com>
2019-05-02 01:50:25 -0700

    Every 10 data from different starting points

--- data-analysis/DataFilters.py
+++ data-analysis/DataFilters.py
@@ -4,6 +4,10 @@ import pandas as pd
 class DataFilter:
     
     def __init__(self):
+        self.source1 = 'C:/earthquake/train.csv'
+        self.source2 = 'F:/myProjects/cmps242/earthquake/data/train.csv'
+        self.destFolder1 = 'C:/earthquake/'
+        self.destFolder2 = 'F:/myProjects/cmps242/earthquake/data/'
         pass
     
     def getPositionalDataInNP( self, df, start, step ):
@@ -13,7 +17,9 @@ class DataFilter:
         """Assumes that positions are preserved across chunks"""
         data = pd.DataFrame()
         for chunk in chunks:
-            data.append( chunk[start::step] )
+            data = data.append( chunk[start::step] )
+            #print( 'chunk shape' + str( chunk.shape ) )
+            #print( 'current shape' + str( data.shape ) )
         return data
             
     def getPositionalDataInNPFromChunks( self, chunks:pd.DataFrame, start, step ):
@@ -23,3 +29,13 @@ class DataFilter:
             dataList.append(chunk.values.tolist())
             
         return np.array(dataList)
+    
+    def saveDF(self, df, filename):
+        df.to_csv( self.destFolder2 + filename )
+        pass
+    
+    def savePositionalDFFromChunks( self, chunks:pd.DataFrame, start, step ):
+        df = self.getPositionalDataFromChunks(chunks, start, step)
+        filename = 'every_' + str(start) + '_from_' + str(step) + '.csv'
+        self.saveDF(df, filename)
+        pass
