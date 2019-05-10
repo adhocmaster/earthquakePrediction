150231556ad06a30ea55ce6d6e716aed346dcad5
adhocmaster <adhocmaster@live.com>
2019-05-10 15:36:52 -0700

    positive bins done. it is super fast. Written some folder structure

---
+++ data-analysis/library/BinJoiner.py
@@ -0,0 +1,3 @@
+# 1. start from an earth quake and go backward. Join every n bins together
+# 2. start anywhere and join a window of n bins till an earthquake
+# 3. Add an especial seperator  for bin time diff. Fill with zeros? experiment.
--- data-analysis/library/BinTransformer.py
+++ data-analysis/library/BinTransformer.py
@@ -1,4 +1,5 @@
 import numpy as np
+import logging, dill, fnmatch, os
 from .Bin import Bin
 from .BinManager import BinManager
 from .BinProcessor import BinProcessor
@@ -8,24 +9,29 @@ class BinTransformer:
     def __init__(self):
         
         self.binManager = BinManager()
-        self.BinProcessor = BinProcessor()
+        self.binProcessor = BinProcessor()
         
         self.numRawBins = 153584
         
         self.destFolderSSD = self.binManager.destFolderSSD
         self.destFolderHDD = self.binManager.destFolderHDD
         
-        self.positiveBinPrefix = 'r_'
+        self.positiveBinPrefix = 'p_'
         self.positiveBinFolder = self.destFolderHDD + 'positive-bins/'
-        
+                
         pass
     
     
     # For positive bins
-    def createPositiveBins(self):
+    def createPositiveBins(self, fromId = 1, toId = 0 ):
         
-        for binId in range(1, self.numRawBins + 1):
-            positiveBin = binProcessor.makeDataPositive( binManager.readRawBinById(binId) )
+        if toId == 0:
+            toId = self.numRawBins 
+        
+        for binId in range(fromId, toId + 1):
+            if (binId % 2000) == 0:
+                print( f'processed {binId}th positive bin' ) 
+            positiveBin = self.binProcessor.makeDataPositive( self.binManager.readRawBinById(binId) )
             self.savePositiveBin( positiveBin )
         
         pass
@@ -42,6 +48,32 @@ class BinTransformer:
     
     def getRelativePositiveFileName(self, binId):
         return self.positiveBinPrefix + 'bin_' + str( binId ) + '.dill'
+        
+    
+    def readPositiveBinById(self, binId):
+        
+        fname = self.positiveBinFolder + self.getRelativePositiveFileName(binId)
+        return self.readPositiveBin(fname)
+    
+        
+    def readPositiveBin(self, fname):
+        
+        with open(fname, 'rb') as f:
+            out = dill.load(f)
+        
+        return out
+    
+    def countPositiveBin(self, fname):
+        
+        return len(os.listdir(self.positiveBinFolder))
+    
+    def readPositiveBins(self, fromId, size):
+        
+        bins = []
+        for i in range(size):
+            bins.append( self.readPositiveBinById(fromId + i) )
+        
+        return bins
             
             
         
