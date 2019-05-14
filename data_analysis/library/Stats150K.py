3c5b14ce793395791add79ba0a9bd5d8936dbb97
Adhocmaster <adhocmaster@live.com>
2019-05-14 16:28:12 -0700

    working on 150K stats

---
+++ data_analysis/library/Stats150K.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+class Stats150K:
+    
+    def __init__(self):
+        pass
+    
+    def createFromDf(self, df, windowSize=150_000, stopAfter = 0, addBinNoToDf = False, dontSaveToDisk = False): 
+        pass
