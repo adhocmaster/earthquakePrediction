91359ef0173c7655b6236fcfcebaf9d28ce07650
adhocmaster <adhocmaster@live.com>
2019-05-13 17:17:42 -0700

    started data generator

--- library/EarthQuakeDataGenerator.py
+++ library/EarthQuakeDataGenerator.py
@@ -1,15 +1,20 @@
 import numpy as np
 import logging, dill, fnmatch, os
 from data_analysis.library.Bin import Bin
+from data_analysis.library.BinIO import BinIO
 from .RegressionDataGenerator import RegressionDataGenerator
 
 class EarthQuakeDataGenerator(RegressionDataGenerator): 
     
-    def __init__(self, batch_size=32, dim=(32,32,32), n_channels=1, shuffle=True):
+    def __init__(self, binType='r', batch_size=32, dim=(32,32,32), n_channels=1, shuffle=True):
     
         # Make IDs here.
     
         super(EarthQuakeDataGenerator, self).__init__(list_IDs, batch_size, dim, n_channels, shuffle)
+        
+        self.binType = binType
+        self.binIO = BinIO()
+        
         pass
     
     
@@ -21,6 +26,10 @@ class EarthQuakeDataGenerator(RegressionDataGenerator):
 
         # Generate data
         for i, ID in enumerate(list_IDs_temp):
+            
+            # read the bin
+            aBin = self.binIO.readBinById(ID, self.binType)
+            
             # Store sample
             X[i,] = np.load('data/' + ID + '.npy')
 
