b33f06d8c21c8645977acdccdfd72af55619fdbd
Adhocmaster <adhocmaster@live.com>
2019-05-14 02:26:11 -0700

    TinyCNN done

--- library/SingleBinDataGenerator.py
+++ library/SingleBinDataGenerator.py
@@ -7,12 +7,13 @@ from embedding.BinEmbedding import *
 
 class SingleBinDataGenerator(RegressionDataGenerator): 
     
-    def __init__(self, binType='nor', embedding='bin', dim=(64,64), batch_size=32, n_channels=1, shuffle=True):
+    def __init__(self, binType='nor', embedding='bin', startBinId = 1, numBins = 153584,  dim=(64,64), batch_size=32, n_channels=1, shuffle=False):
     
         self.binType = binType
         self.embedding = embedding
         self.binIO = BinIO()
-        self.numRawBins = 153584
+        self.startBinId = startBinId
+        self.numBins = numBins
 
         # Make IDs here.
         list_IDs = self.getListIds()
@@ -25,7 +26,7 @@ class SingleBinDataGenerator(RegressionDataGenerator):
     def getListIds(self):
 
         if self.embedding == 'bin':
-            return list(range(1, self.numRawBins +1))
+            return list(range(self.startBinId, self.numBins +1))
     
 
     def getEmbedder(self):
@@ -33,6 +34,20 @@ class SingleBinDataGenerator(RegressionDataGenerator):
        if self.embedding == 'bin':
            return BinEmbedding(4096)
 
+
+    def __getitem__(self, index):
+
+        'Generate one batch of data'
+        # Generate indexes of the batch
+        indexes = self.indexes[index*self.batch_size:(index+1)*self.batch_size]
+
+        # Find list of IDs
+        list_IDs_temp = [self.list_IDs[k] for k in indexes]
+
+        # Generate data
+        X, y = self.__data_generation(list_IDs_temp)
+
+        return X, y
 
     def __data_generation(self, list_IDs_temp):
         'Generates data containing batch_size samples' # X : (n_samples, *dim, n_channels)
@@ -51,6 +66,9 @@ class SingleBinDataGenerator(RegressionDataGenerator):
 
             # Store class
             y[i] = aBin.ttf
+
+            print( X.shape )
+            print( y.shape )
 
         return X, y
     
