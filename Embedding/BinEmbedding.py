da92bbe4278b230cc7d17b1bdbd5d41cdbd5a5c0
adhocmaster <adhocmaster@live.com>
2019-05-13 18:34:33 -0700

    need to normalize data first. added some stats

--- Embedding/BinEmbedding.py
+++ Embedding/BinEmbedding.py
@@ -5,6 +5,13 @@ from .Embedding import Embedding
 class BinEmbedding(Embedding):
     
     def __init__(self, binSize = 4096 ):
+        
+        self.binSize = 4096
+        self.rowDim = 64
+        self.colDim = int( self.binSize / self.rowDim )
+        
+        if self.binSize % self.rowDim != 0:
+            raise Exception(f"{binSize} is not divisible by {self.rowDim}")
         pass
     
     def fromBin(self, aBin: Bin):
@@ -16,6 +23,6 @@ class BinEmbedding(Embedding):
         else if curBinSize > 4096:
             aBin = self.reduceBinWithQuake(aBin, 4096)
         
-        
+        return aBin.data.reshape([self.rowDim,self.colDim])
             
         
